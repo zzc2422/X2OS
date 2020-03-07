c814a6f2a93ca07129af4c9ea1f63642eeb607a2
zzc2422 <liksasdilasely@163.com>
2020-03-07 13:10:56 +0800

    添加defs.h

---
+++ src/libs/defs.h
@@ -0,0 +1,8 @@
+typedef u8 unsigned char;
+typedef i8 signed char;
+typedef u16 unsigned short;
+typedef i16 signed short;
+typedef u32 unsigned int;
+typedef i32 signed int;
+typedef u64 unsigned long long;
+typedef i64 signed long long;
