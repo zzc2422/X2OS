c87f0706c0fe2201c62d92572e13317123d17d16
zzc2422 <liksasdilasely@163.com>
2020-06-16 00:03:49 +0800

    调整项目结构

--- run.sh
+++ run.sh
@@ -17,13 +17,13 @@ link=scp/link.ld
 gdb_init=scp/gdb_init
 sbi=ext/$machine"_sbi"
 
-rm -rf tmp bin
+rm -rf bin/*
 
 all_dirs=$(find src -type d)
 lib_opt=""
 for src_dir in $all_dirs
 do
-	out_dir="tmp"${src_dir: 3}
+	out_dir="bin"${src_dir: 3}
 	mkdir $out_dir -p
 	lib_opt=$lib_opt" -I"$src_dir
 done
@@ -32,7 +32,7 @@ all_files=$(find src -type f)
 for src_file in $all_files
 do
 	ext=${src_file##*.}
-	out_file="tmp"${src_file: 3}".o"
+	out_file="bin"${src_file: 3}".o"
 	if [ $ext == "c" ]
 	then
 		if [ ${src_file: 0: 15} == "src/boot/part1/" ]
@@ -47,9 +47,8 @@ do
 	fi
 done
 
-mkdir bin -p
-o_files=$(find tmp -type f)
-$ld $o_files -T $link -o tmp/os.bin -m elf32lriscv
-$objcopy tmp/os.bin bin/os -O binary
+o_files=$(find -L bin -type f)
+$ld $o_files -T $link -o bin/os.bin -m elf32lriscv
+$objcopy bin/os.bin bin/os -O binary
 $qemu --machine $machine --bios $sbi -device loader,file=bin/os,addr=0x80400000 -S -s &
 $gdb -x $gdb_init
